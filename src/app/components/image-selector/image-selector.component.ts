4609782d471cfab8c32c5db06ce2cc11a3f501f6
Adrian Albu <2412rock@gmail.com>
2024-04-02 23:35:49 +0300

    Modernize image selector

--- src/app/components/image-selector/image-selector.component.ts
+++ src/app/components/image-selector/image-selector.component.ts
@@ -1,6 +1,7 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Imagine } from 'src/app/model/Imagine';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-image-selector',
@@ -8,48 +9,75 @@ import { Imagine } from 'src/app/model/Imagine';
   styleUrl: './image-selector.component.css'
 })
 export class ImageSelectorComponent {
-  @Output() outPutImages:EventEmitter<Imagine[]> = new EventEmitter<Imagine[]>();
+  @Output() outPutImages: EventEmitter<Imagine[]> = new EventEmitter<Imagine[]>();
   @Input() selectedImages: Imagine[];
+  @ViewChild('fileInput') inputRef: ElementRef<HTMLInputElement>;
+  constructor(private modalService: ModalService) { }
 
-  private getFileExtension(fileName: string): string{
+  private getFileExtension(fileName: string): string {
     return fileName.split('.').pop() || '';
+  }
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.selectedImages, event.previousIndex, event.currentIndex);
+    this.outPutImages.emit(this.selectedImages);
+  }
+
+  onSubmit(): void {
+
+  }
+
+  addFiles(files: FileList | null): void {
+    console.log(files);
+    //this.filesChanged.emit(files);
+  }
+
+  handleFileDrop(event: DragEvent | null) {
+    if (event?.dataTransfer?.files?.length) {
+      const files = event.dataTransfer.files;
+      this.inputRef.nativeElement.files = files;
+      if(files.length > 10){
+        this.modalService.openModalNotification("Error", "Please select only 10 images", false);
+        return;
+      }
+      this.readFilesData(files);
+    }
+  }
+
+  readFilesData(files: any){
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = () => {
+        var img = new Imagine();
+        img.fileContentBase64 = reader.result as string;
+        img.fileExtension = this.getFileExtension(file.name);
+        img.fileType = file.type;
+        this.selectedImages.push(img);
+      };
+      reader.readAsDataURL(file);
+    }
+    this.outPutImages.emit(this.selectedImages);
   }
 
   onFileSelected(event: any) {
     const files = event.target.files;
     if (files) {
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            const reader = new FileReader();
-            reader.onload = () => {
-              var img = new Imagine();
-              img.fileContentBase64 = reader.result as string;
-              img.fileExtension = this.getFileExtension(file.name);
-              img.fileType = file.type;
-              this.selectedImages.push(img);
-            };
-            reader.readAsDataURL(file);
-        }
-        this.outPutImages.emit(this.selectedImages);
+      if (files.length > 10) {
+        this.modalService.openModalNotification("Error", "Please select only 10 images", false);
+        return;
+      }
+      this.readFilesData(files);
     }
-}
+  }
 
-removeImage(item: any) {
+   removeImage(item: any) {
     const index = this.selectedImages.indexOf(item);
     if (index !== -1) {
-        this.selectedImages.splice(index, 1);
+      this.selectedImages.splice(index, 1);
     }
     this.outPutImages.emit(this.selectedImages);
-}
-
-
-drop(event: CdkDragDrop<string[]>) {
-  moveItemInArray(this.selectedImages, event.previousIndex, event.currentIndex);
-  this.outPutImages.emit(this.selectedImages);
-}
-
-onSubmit(): void {
-    
-}
+  }
 
 }
