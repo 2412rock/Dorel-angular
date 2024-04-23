a2b166d803bf5314fd9bc05908c46b41a3546b2c
Adrian Albu <2412rock@gmail.com>
2024-04-23 10:47:01 +0300

    Admin dashboard show emails

--- src/app/components/overview-admin/overview-admin.component.ts
+++ src/app/components/overview-admin/overview-admin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { DBAccessLogModel } from 'src/app/model/DBModels/DbAccessLogModel';
 import { AccessLogsService } from 'src/app/services/access-logs.service';
+import { DataService } from 'src/app/services/data.service';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -12,10 +13,18 @@ import { ModalService } from 'src/app/services/modal.service';
 export class OverviewAdminComponent {
 
   public logs: DBAccessLogModel[];
+  public emails: string[] = [];
+  public dropdownValues = ["Ip addresses", "Emails"]
+  public selectedDropdownValue = this.dropdownValues[0];
+  public showDropDown: boolean = false;
 
-  constructor(private accessLogsService: AccessLogsService,private modalService: ModalService){}
+  constructor(private accessLogsService: AccessLogsService,private modalService: ModalService, private dataService: DataService){}
 
   ngOnInit(){
+    this.getLogs();
+  }
+
+  getLogs(){
     firstValueFrom(this.accessLogsService.getLogs()).then(e => {
       if(e.isSuccess){
         this.logs = e.data;
@@ -25,4 +34,26 @@ export class OverviewAdminComponent {
       }
     }).catch(e => this.modalService.openModalNotification("Error", "Cant get logs", false))
   }
+
+  getAllUsers(){
+    firstValueFrom(this.dataService.getAllUsers()).then(e => {
+      if(e.isSuccess){
+        this.emails = e.data;
+      }
+      else{
+        this.modalService.openModalNotification("Error", "Cant get users", false);
+      }
+    }).catch(e => this.modalService.openModalNotification("Error", "Cant get users", false))
+  }
+
+  selectItem(index: number){
+    this.selectedDropdownValue = this.dropdownValues[index];
+    this.showDropDown = false;
+    if(index === 0){
+      this.getLogs();
+    }else{
+      this.getAllUsers();
+    }
+  }
+
 }
